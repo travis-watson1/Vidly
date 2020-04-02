c62174468eaeb80c37d6d95e4cc425bb6a512731
Travis Watson <watsonjamest@gmail.com>
2020-04-02 12:57:33 -0500

    Add attribute routing to project

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -11,8 +11,36 @@ namespace Vidly.Controllers
     {
         public IActionResult Random()
         {
-            var movie = new Movie() {Name = "Shrek!"};
+            var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
         }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+
+        [Route("movies/released/{year:regex(\\d{{4}})}/{month:regex(\\d{{2}}):range(1,12)}")]
+        public IActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+
+
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace Vidly
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -60,6 +61,7 @@ namespace Vidly
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
