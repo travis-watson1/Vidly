84cc5a0c5684b171cbc018d212cf5e9fb7a52c68
travis-watson1 <watsonjamest@gmail.com>
2020-06-14 23:10:04 -0500

    Add HttpPost Create Action for Customers controller

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -45,6 +45,16 @@ namespace Vidly.Controllers
             };
 
             return View(viewModel);
+        }        
+        
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
         }
 
     }
